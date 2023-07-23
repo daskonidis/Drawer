ab508862030d63d03b40ca9f23faf847a4de54f3
daskonidis <daskonidis@gmail.com>
2023-07-23 18:58:08 +0300

    Initial upload

---
+++ Drawer/frmMain.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Drawer
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbPrinters = new System.Windows.Forms.ComboBox();
+            this.btnOpenDrawer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmbPrinters
+            // 
+            this.cmbPrinters.FormattingEnabled = true;
+            this.cmbPrinters.Location = new System.Drawing.Point(68, 29);
+            this.cmbPrinters.Name = "cmbPrinters";
+            this.cmbPrinters.Size = new System.Drawing.Size(395, 21);
+            this.cmbPrinters.TabIndex = 0;
+            // 
+            // btnOpenDrawer
+            // 
+            this.btnOpenDrawer.Location = new System.Drawing.Point(188, 73);
+            this.btnOpenDrawer.Name = "btnOpenDrawer";
+            this.btnOpenDrawer.Size = new System.Drawing.Size(155, 38);
+            this.btnOpenDrawer.TabIndex = 1;
+            this.btnOpenDrawer.Text = "Open Drawer";
+            this.btnOpenDrawer.UseVisualStyleBackColor = true;
+            this.btnOpenDrawer.Click += new System.EventHandler(this.btnOpenDrawer_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 156);
+            this.Controls.Add(this.btnOpenDrawer);
+            this.Controls.Add(this.cmbPrinters);
+            this.Name = "frmMain";
+            this.Text = "Drawer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbPrinters;
+        private System.Windows.Forms.Button btnOpenDrawer;
+    }
+}
+
---
+++ Drawer/frmMain.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Drawer
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+            PopulatePrinters();
+        }
+
+        private void PopulatePrinters()
+        {
+            foreach (string printer in System.Drawing.Printing.PrinterSettings.InstalledPrinters)
+                cmbPrinters.Items.Add(printer);
+        }
+
+        private void btnOpenDrawer_Click(object sender, EventArgs e)
+        {
+            StringBuilder command = new StringBuilder();
+            command.Append((char)27);
+            command.Append((char)112);
+            command.Append((char)0);
+            command.Append((char)25);
+            command.Append((char)255);
+            RawPrinterHelper.SendStringToPrinter(cmbPrinters.Text, command.ToString());
+        }
+    }
+}
